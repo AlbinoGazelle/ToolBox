8c85ca59223a4ef8b147d4d48994542129e4f9fd
AlbinoGazelle <nburns9922@gmail.com>
2022-03-15 23:13:51 -0700

    v1.0 of virustotal command w/ alright error handling and ability to submit URLs for analysis

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from urllib import response
+from time import sleep
 import discord #duhhhh
 import logging #used for useful logging output (and checking for heartbeat)
 import json #used for parsing config.json to avoid keys being leaked to github
@@ -8,6 +8,8 @@ from discord.ext import commands
 from datetime import datetime
 import requests
 import hashlib
+import base64
+from urllib.parse import urlparse
 #setup logging
 logging.basicConfig(
     level = logging.INFO,
@@ -30,6 +32,15 @@ async def on_ready():
     await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f"👁️"))
     print(f'Bot connected as {bot.user}')
 
+#validate urls
+def is_url(url):
+  try:
+    result = urlparse(url)
+    return all([result.scheme, result.netloc])
+  except ValueError:
+    return False
+
+
 @bot.command(pass_context=True)
 async def pfp(ctx):
     try:
@@ -40,25 +51,79 @@ async def pfp(ctx):
     except IndexError:
         await ctx.send("You must mention a user!")
 
-@bot.command(pass_context=True)
-async def vt(ctx):
-    try:
-        data = ctx.message.content.split()
-        data = hashlib.sha256(str.encode(data[1]))
-        url = f"https://www.virustotal.com/api/v3/urls/{data.hexdigest()}"
-        headers = {
+
+# Returns base64 representation of a given URL without padding
+# Parameters:
+#  url: Valid HTTP URL
+#    type: string
+# Returns:
+#  base64 representation of url 
+#    type: string
+
+async def get_url(url):
+    return base64.b64encode(url.encode('ascii')).decode('ascii').strip("=")
+
+# Submits a GET request to VirusTotal's API to receive data on a URL
+# Parameters: 
+#  url: virustotal API url with base64 encode URL string as id
+#    type: string
+# Returns:
+#  HTTP response
+#   type: response object
+async def get_vt_url_info(url):
+    headers = {
+        "Accept" : "application/json",
+        "x-apikey" : config['vt']
+    }
+    response = requests.request("GET", url, headers=headers)
+    return response
+
+# Submits a POST request to VirusTotal's API to submit a URL for analysis
+# Parameters:
+#  url: virustotal API endpoint
+#    type: string
+#  data: URL to submit to VirusTotal
+#    type: string
+# Returns:
+#  HTTP response:
+#   type: response object
+async def submit_url(url, data):
+    headers = {
             "Accept" : "application/json",
             "x-apikey" : config['vt']
-        }
-        response = requests.request("GET", url, headers=headers)
-        vt_data = response.json().get('data').get('attributes').get('last_analysis_stats')
+    }
+    print(f"submitting :{data}")
+    response = requests.request("POST", url, headers=headers, data=data)
+    return response
+
+@bot.command(pass_context=True)
+async def vt(ctx):
+    normalized_url = ctx.message.content.split()
+    base64Url = await get_url(normalized_url[1])
+    url = f'https://www.virustotal.com/api/v3/urls/{base64Url}'
+    response = await get_vt_url_info(url)
+    #try to get data, if errors means either:
+    #1. VT hasnt seen this URL or
+    #2. URL isnt valid
+    try:
+        #get last analysis stats if we've seen the URL before
+        vt_data = response.json()['data']['attributes']['last_analysis_stats']
+            #get total number of scans
+        total_scans = int(vt_data.get('harmless')) + int(vt_data.get('malicious')) + int(vt_data.get('suspicious')) + int(vt_data.get('undetected'))
+        #calculate detection rate
+        detection_rate = round(int(vt_data.get('malicious')) + int(vt_data.get('suspicious')) / total_scans, 2)
+        #change embed color if we have detections
+        if detection_rate > 0:
+            color = Color.red()
+        else:
+            color = Color.blue()
+        #craft embed and add fields
         embed = discord.Embed(
             title = "VirusTotal Detection Rates",
-            color = Color.blue()
+            color = color
         )
-        total_scans = int(vt_data.get('harmless')) + int(vt_data.get('malicious')) + int(vt_data.get('suspicious')) + int(vt_data.get('undetected'))
-        detection_rate = round(int(vt_data.get('malicious')) + int(vt_data.get('suspicious')) / total_scans, 2)
-        embed.add_field(name="Link:", value=f"https://www.virustotal.com/gui/url/{data.hexdigest()}")
+        embed.set_image(url="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b7/VirusTotal_logo.svg/564px-VirusTotal_logo.svg.png")
+        embed.add_field(name="Link:", value=f"https://www.virustotal.com/gui/url/{base64Url}")
         embed.add_field(name="Harmless:", value=vt_data.get('harmless'))
         embed.add_field(name="Malicious:", value=vt_data.get('malicious'))
         embed.add_field(name="Suspicious:", value=vt_data.get('suspicious'))
@@ -66,8 +131,24 @@ async def vt(ctx):
         embed.add_field(name="Total:", value=total_scans)
         embed.add_field(name="Detection Rate:", value=f"{detection_rate}%")
         await ctx.send(embed=embed)
-    except:
-        await ctx.send("You did something wrong, better error handling soontm")
+    except KeyError as e:
+        await ctx.send(f"VirusTotal has not seen this URL. Submitting it now... please wait.")
+        vt_url = f"https://www.virustotal.com/api/v3/urls"
+        data = {'url': normalized_url[1]}
+        submitData = await submit_url(vt_url, data)
+        sleep(20)
+        print(submitData.text)
+        try:
+            if submitData.json()['error']['code'] == "InvalidArgumentError":
+                await ctx.send(f"Virustotal cannot process this URL! Check for typos! Reason: `{submitData.json()['error']['message']}`")
+                return
+            else:
+                await ctx.send(f"{ctx.author.mention} Analysis Complete!")
+                await vt(ctx)
+        except KeyError:
+            await ctx.send(f"{ctx.author.mention} Analysis Complete!")
+            await vt(ctx)
+
 #send message to logging channel when a message is deleted
 @bot.event
 async def on_message_delete(message):
@@ -147,4 +228,3 @@ async def on_voice_state_update(member, before, after):
 
 #start the bot
 bot.run(config['token'])
-
